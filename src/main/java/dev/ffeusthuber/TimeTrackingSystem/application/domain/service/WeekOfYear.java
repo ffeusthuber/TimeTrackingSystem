52992592d877ec75e54bc8ca32cb2c7d1c440cec
ffeusthuber <ffeusthu@gmail.com>
2024-12-01 10:04:45 +0100

    refactored WeekOfYear to record

--- src/main/java/dev/ffeusthuber/TimeTrackingSystem/application/domain/service/WeekOfYear.java
+++ src/main/java/dev/ffeusthuber/TimeTrackingSystem/application/domain/service/WeekOfYear.java
@@ -3,20 +3,14 @@ package dev.ffeusthuber.TimeTrackingSystem.application.domain.service;
 import java.time.LocalDate;
 import java.time.temporal.WeekFields;
 
-public class WeekOfYear {
-    private final int weekNumber;
-    private final int year;
-
-    public WeekOfYear(int weekNumber, int year) {
+public record WeekOfYear(int weekNumber, int year) {
+    public WeekOfYear {
         validateWeekNumber(weekNumber, year);
-
-        this.weekNumber = weekNumber;
-        this.year = year;
     }
 
     private void validateWeekNumber(int weekNumber, int year) {
         int numberOfWeeksInYear = getNumberOfWeeksInYear(year);
-        if(weekNumber < 1 || weekNumber > numberOfWeeksInYear){
+        if (weekNumber < 1 || weekNumber > numberOfWeeksInYear) {
             throw new IllegalArgumentException("Week number must be between 1 and " + numberOfWeeksInYear + " for year " + year);
         }
     }
@@ -25,17 +19,9 @@ public class WeekOfYear {
         LocalDate lastDayOfYear = LocalDate.of(year, 12, 31);
         int lastWeekOfYear = lastDayOfYear.get(WeekFields.ISO.weekOfWeekBasedYear());
         //if the last day of the year falls in the first week of the next year
-        if(lastWeekOfYear == 1){
+        if (lastWeekOfYear == 1) {
             return lastDayOfYear.minusWeeks(1).get(WeekFields.ISO.weekOfWeekBasedYear());
         }
         return lastWeekOfYear;
-    }
-
-    public int getWeekNumber() {
-        return weekNumber;
-    }
-
-    public int getYear() {
-        return year;
     }
 }
